48c672d34b3361a4787d8d14ad4395e0e9d15a74
Sardorbek <axadjonovsardorbeck@gmail.com>
2024-09-25 14:24:04 +0500

    MiniTwitter started

---
+++ auth_service/migrations/000001_create_table.up.sql
@@ -0,0 +1,47 @@
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(64) NOT NULL,
+    username VARCHAR(64) NOT NULL,
+    bio VARCHAR(128) DEFAULT NULL,
+    image_url VARCHAR(255) DEFAULT NULL,
+    password VARCHAR(64) NOT NULL,
+    email VARCHAR(64) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at BIGINT NOT NULL DEFAULT 0,
+    UNIQUE(email, deleted_at),
+    UNIQUE(username, deleted_at)
+);
+
+CREATE TABLE IF NOT EXISTS twits (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES users(id) NOT NULL,
+    twit_id UUID REFERENCES twits(id) DEFAULT NULL,
+    content VARCHAR(255) NOT NULL,
+    image_url VARCHAR(255) DEFAULT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at BIGINT NOT NULL DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS follows (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    follower_id UUID REFERENCES users(id) NOT NULL,
+    followed_id UUID REFERENCES users(id) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at BIGINT NOT NULL DEFAULT 0,
+    UNIQUE(follower_id, deleted_at),
+    UNIQUE(followed_id, deleted_at)
+);
+
+CREATE TABLE IF NOT EXISTS likes (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES users(id) NOT NULL,
+    twit_id UUID REFERENCES twits(id) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at BIGINT NOT NULL DEFAULT 0,
+    UNIQUE(user_id, deleted_at),
+    UNIQUE(twit_id, deleted_at)
+);
